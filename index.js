260b6e7282f2773da7c2dce376886c0cd4dc101c
AduEmmaaa <isotopedegeneral@gmail.com>
2024-03-04 16:21:01 +0000

    progrss

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const btn1 = document.getElementById("icona");
 const card = document.getElementById("dropdown");
+const showbtn = document.getElementById("showbtn");
+const showbtn1 = document.getElementById("showbtn1");
+const para = document.getElementById("para");
+const para1 = document.getElementById("para1");
 
 btn1.addEventListener("click", () => {
   if (card.style.display === "block") {
     card.style.display = "none";
   } else {
     card.style.display = "block";
+  }
+});
+
+showbtn.addEventListener("click", () => {
+  if (para.style.display === "block") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+});
+
+showbtn1.addEventListener("click", () => {
+  if (para1.style.display === "block") {
+    para1.style.display = "none";
+  } else {
+    para1.style.display = "block";
   }
 });
